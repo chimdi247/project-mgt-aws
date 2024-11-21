7953f20a76b606a70c46221e045c5f9a73250e00
chimdi247 <Chimdi247@gmail.com>
2024-11-21 23:46:26 +0200

    finished authentication with cognito

--- server/src/index.ts
+++ server/src/index.ts
@@ -35,6 +35,7 @@ app.use("/teams", teamRoutes);
 
 /* SERVER */
 const port = Number(process.env.PORT) || 3000;
+
 app.listen(port, "0.0.0.0", () => {
   console.log(`Server running on part ${port}`);
 });
